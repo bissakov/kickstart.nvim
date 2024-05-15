d1124680943c740d3707666c66cdba69a79a211b
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-05-16 00:22:53 +0500

    Remove comments

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -83,8 +83,6 @@ return {
 
     require('dap-go').setup {
       delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
         detached = vim.fn.has 'win32' == 0,
       },
     }
