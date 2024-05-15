85ce44d0922368081d43573e5ccc66fde69e10cf
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-05-16 00:20:25 +0500

    Add diff to treesitter's ensure_installed languages

--- init.lua
+++ init.lua
@@ -430,7 +430,17 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'vim',
+        'vimdoc',
+      },
       auto_install = true,
       highlight = {
         enable = true,
