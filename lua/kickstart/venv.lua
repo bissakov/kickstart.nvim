b281521b1a607110bf57732e7ebbccc542cbd8c5
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-06-03 17:17:40 +0500

    Simplify venv configuration

--- lua/kickstart/venv.lua
+++ lua/kickstart/venv.lua
@@ -1,21 +1,28 @@
 local M = {}
 
 function M.setup()
-  local utils = require 'kickstart.utils'
-  local vimrc = os.getenv 'MYVIMRC' or vim.fn.stdpath 'config' .. '/init.lua'
-  local config_path = vim.fn.fnamemodify(vimrc, ':h')
-  local venv = config_path .. '/venv'
+  local venv = vim.fn.stdpath 'config' .. '/venv'
   local is_win32 = vim.fn.has 'win32' == 1
-  local bin_path = utils.ternary(is_win32, venv .. '/Scripts', venv .. '/bin')
-  local pip = utils.ternary(is_win32, bin_path .. '/pip.exe', venv .. '/pip')
+  local bin_path = ''
+  local pip = ''
+  local py = ''
+
+  if is_win32 then
+    bin_path = venv .. '/Scripts'
+    pip = bin_path .. '/pip.exe'
+    py = bin_path .. '/python.exe'
+  else
+    bin_path = venv .. '/bin'
+    pip = venv .. '/pip'
+    py = bin_path .. '/python'
+  end
 
   if not vim.fn.isdirectory(venv) then
     vim.cmd('!py -3.11 -m venv ' .. venv)
     vim.cmd('!' .. pip .. ' install neovim')
   end
 
-  vim.g.python3_host_prog =
-    utils.ternary(is_win32, bin_path .. '/python.exe', bin_path .. '/python')
+  vim.g.python3_host_prog = py
 end
 
 return M
