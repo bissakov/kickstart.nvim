4b6af611e1305701f08a748e462c77692bdf48b6
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-06-03 15:24:29 +0500

    Cross-platform Python venv configuration

--- init.lua
+++ init.lua
@@ -10,17 +10,21 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 
+local utils = require 'kickstart.utils'
 local vimrc = os.getenv 'MYVIMRC' or vim.fn.stdpath 'config' .. '/init.lua'
-local current_folder = vim.fn.fnamemodify(vimrc, ':h')
-local venv = current_folder .. '\\venv'
-local pip = venv .. '\\Scripts\\pip.exe'
+local config_path = vim.fn.fnamemodify(vimrc, ':h')
+local venv = config_path .. '/venv'
+local is_win32 = vim.fn.has 'win32' == 1
+local bin_path = utils.ternary(is_win32, venv .. '/Scripts', venv .. '/bin')
+local pip = utils.ternary(is_win32, bin_path .. '/pip.exe', venv .. '/pip')
 
 if not vim.fn.isdirectory(venv) then
   vim.cmd('!py -3.11 -m venv ' .. venv)
   vim.cmd('!' .. pip .. ' install neovim')
 end
 
-vim.g.python3_host_prog = venv .. '\\Scripts\\python.exe'
+vim.g.python3_host_prog =
+  utils.ternary(is_win32, bin_path .. '/python.exe', bin_path .. '/python')
 
 --  NOTE: Options
 
