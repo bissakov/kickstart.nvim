68ad261563ad06d1ba73b0044ac3aa7ad06d7ae6
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-06-04 20:41:17 +0500

    Reworked format.lua

--- lua/kickstart/format.lua
+++ lua/kickstart/format.lua
@@ -1,31 +1,30 @@
-vim.api.nvim_create_autocmd('BufWritePost', {
-  desc = 'Format on save',
-  pattern = { '*.py', '*.lua', '*.json' },
-  group = vim.api.nvim_create_augroup('custom-auto-format', { clear = true }),
-  callback = function()
-    local mason_registry = require 'mason-registry'
-    local current_file = vim.fn.expand '%:p'
+local is_windows = vim.fn.has 'win32' == 1
 
-    local file_ext = vim.fn.expand '%:e'
-
-    if file_ext == 'py' then
+local formatters = {
+  py = {
+    cmd = function(mason_registry, current_file)
       local isort = mason_registry.get_package('isort'):get_install_path()
       local ruff = mason_registry.get_package('ruff'):get_install_path()
 
-      local isort_exe = isort .. '\\venv\\Scripts\\isort.exe'
-      local ruff_exe = ruff .. '\\venv\\Scripts\\ruff.exe'
+      local bin_path = is_windows and '/Scripts' or '/bin'
+      local isort_exe = isort .. '/venv' .. bin_path .. '/isort.exe'
+      local ruff_exe = ruff .. '/venv' .. bin_path .. '/ruff.exe'
 
       local args = {
         '--line-length 80',
       }
 
-      vim.cmd('silent !' .. isort_exe .. ' ' .. current_file)
-      vim.cmd(
-        'silent !' .. ruff_exe .. ' format ' .. current_file .. ' ' .. table.concat(args, ' ')
-      )
-    elseif file_ext == 'lua' then
+      local isort_cmd = isort_exe .. ' ' .. current_file
+      local ruff_cmd = ruff_exe .. ' format ' .. current_file .. ' ' .. table.concat(args, ' ')
+      local cmd = isort_cmd .. ' && ' .. ruff_cmd
+
+      return cmd
+    end,
+  },
+  lua = {
+    cmd = function(mason_registry, current_file)
       local stylua = mason_registry.get_package('stylua'):get_install_path()
-      local stylua_exe = stylua .. '\\stylua.exe'
+      local stylua_exe = stylua .. (is_windows and '/stylua.exe' or '/stylua')
 
       local args = {
         '--indent-width 2',
@@ -37,13 +36,31 @@ vim.api.nvim_create_autocmd('BufWritePost', {
         '--line-endings Unix',
       }
 
-      vim.cmd(
-        'silent !' .. stylua_exe .. ' ' .. current_file .. ' ' .. table.concat(args, ' ')
-      )
-    elseif file_ext == 'json' then
-      local jq_exe = mason_registry.get_package('jq'):get_install_path()
-        .. '\\jq-windows-amd64.exe'
-      vim.cmd('silent !' .. jq_exe .. ' . ' .. current_file .. ' > ' .. current_file .. '.tmp')
-    end
+      local cmd = stylua_exe .. ' ' .. current_file .. ' ' .. table.concat(args, ' ')
+
+      return cmd
+    end,
+    json = {
+      cmd = function(mason_registry, current_file)
+        local jq = mason_registry.get_package 'jq'
+        local jq_exe = jq:get_install_path() .. '/jq-windows-amd64.exe'
+        local cmd = jq_exe .. ' . ' .. current_file .. ' > ' .. current_file .. '.tmp'
+        return cmd
+      end,
+    },
+  },
+}
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+  desc = 'Format on save',
+  pattern = { '*.py', '*.lua', '*.json' },
+  group = vim.api.nvim_create_augroup('custom-auto-format', { clear = true }),
+  callback = function()
+    local mason_registry = require 'mason-registry'
+    local current_file = vim.fn.expand '%:p'
+
+    local file_ext = vim.fn.expand '%:e'
+    local cmd = formatters[file_ext].cmd(mason_registry, current_file)
+    vim.cmd('silent !' .. cmd)
   end,
 })
