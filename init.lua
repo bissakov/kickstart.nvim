bf5680b3a809f485c2431ef256bbe268e7a69b14
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-06-10 16:03:28 +0500

    New keymap for duplicating current line down

--- init.lua
+++ init.lua
@@ -55,6 +55,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<C-d>', 'yyp', { desc = 'Duplicate current line [D]own' })
+
 vim.keymap.set(
   'n',
   '<leader>df',
