c87ba06957826419c1c00a94fc7b3169d94e20ac
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-06-12 14:55:38 +0500

    Removed comments and added missing-fields diagnostic to dapui setup

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -1,19 +1,10 @@
 return {
-  -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
   dependencies = {
-    -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
-
-    -- Required dependency for nvim-dap-ui
     'nvim-neotest/nvim-nio',
-
-    -- Installs the debug adapters for you
     'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
-
-    -- Add your own debuggers here
     'leoluz/nvim-dap-go',
     'mfussenegger/nvim-dap-python',
   },
@@ -37,7 +28,6 @@ return {
         end,
         desc = 'Debug: Set Breakpoint',
       },
-
       { '<F7>', dapui.toggle, desc = 'Debug: See last session result.' },
       {
         '<leader>e',
@@ -46,6 +36,7 @@ return {
         end,
         desc = 'Debug: Evaluate expression',
       },
+
       unpack(keys),
     }
   end,
@@ -54,30 +45,18 @@ return {
     local dapui = require 'dapui'
 
     require('mason-nvim-dap').setup {
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
       automatic_installation = true,
-
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
       handlers = {},
-
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
       ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
         'python',
       },
     }
 
-    -- Dap UI setup
-    -- For more information, see |:help nvim-dap-ui|
+    --- @diagnostic disable-next-line: missing-fields
     dapui.setup {
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      --- @diagnostic disable-next-line: missing-fields
       controls = {
         icons = {
           pause = '⏸',
