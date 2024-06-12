f95e8996c11e4f9969ef5e9d8281aed431d0f9ee
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-06-12 14:54:03 +0500

    Moved custom formatting configurations into 'stevearc/conform.nvim' config to make it lazy-loadable

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -32,4 +32,68 @@ return {
       c = { 'clang-format' },
     },
   },
+  config = function(_, opts)
+    local conform = require 'conform'
+    conform.setup(opts)
+
+    --  NOTE: Python formatters
+    conform.formatters.isort = {
+      args = function(_, _)
+        return {
+          '--stdout',
+          '--filename',
+          '$FILENAME',
+          '-',
+        }
+      end,
+    }
+
+    -- HACK: This hack works (overriding args)
+    conform.formatters.ruff_format = {
+      args = {
+        'format',
+        '--line-length',
+        '80',
+        '--force-exclude',
+        '--stdin-filename',
+        '$FILENAME',
+        '-',
+      },
+    }
+
+    --  NOTE: Lua formatters
+    conform.formatters.stylua = {
+      prepend_args = {
+        '--column-width',
+        '95',
+        '--line-endings',
+        'Unix',
+        '--indent-type',
+        'Spaces',
+        '--indent-width',
+        '2',
+        '--quote-style',
+        'AutoPreferSingle',
+        '--call-parentheses',
+        'None',
+      },
+    }
+
+    --  NOTE: C formatters
+    conform.formatters.clang_format = {
+      args = function(_, _)
+        return {
+          '-style',
+          'google',
+        }
+      end,
+    }
+
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      pattern = '*',
+      callback = function(args)
+        conform.format { bufnr = args.buf }
+      end,
+    })
+  end,
 }
