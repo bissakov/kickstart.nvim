53b5e1a670f38504d8ab7b2027a652bcf5290510
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-06-14 16:49:24 +0500

    Skip unknown treesitter languages

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -1,11 +1,26 @@
 local supported_langs = require 'kickstart.supported_langs'
 
+--- Get the languages that are supported by treesitter
+--- @param langs string[]
+--- @return string[]
+local function get_supported_languages(langs)
+  local parsers = require 'nvim-treesitter.parsers'
+  local supported = {}
+
+  for _, lang in ipairs(langs) do
+    if parsers.get_parser_configs()[lang] then
+      table.insert(supported, lang)
+    end
+  end
+
+  return supported
+end
+
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   ft = supported_langs,
   opts = {
-    ensure_installed = supported_langs,
     auto_install = true,
     incremental_selection = {
       enable = true,
@@ -20,6 +35,9 @@ return {
   },
   config = function(_, opts)
     require('nvim-treesitter.install').prefer_git = true
+
+    opts.ensure_installed = get_supported_languages(supported_langs)
+
     ---@diagnostic disable-next-line: missing-fields
     require('nvim-treesitter.configs').setup(opts)
   end,
