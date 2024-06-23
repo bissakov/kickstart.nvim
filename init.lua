227238577e92158a27b6e9435e3fd210b446e4ff
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-06-23 13:16:56 +0500

    (feat): Treat keywords with `-` as one keyword

--- init.lua
+++ init.lua
@@ -30,6 +30,7 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 vim.opt.scrolloff = 8
 vim.opt.smartindent = true
+vim.opt.iskeyword:append { '-' }
 
 require('kickstart.py_env').setup()
 require 'kickstart.mappings'
