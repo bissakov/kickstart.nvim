ec2d47b6f18038961863ecd2fa348aece4eb861a
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-06-24 22:41:27 +0500

    (feat): Merge tables helper function

--- lua/kickstart/utils.lua
+++ lua/kickstart/utils.lua
@@ -33,4 +33,15 @@ function M.setTimeout(timeout)
   return timer
 end
 
+--- Merge two tables
+--- @param t1 table
+--- @param t2 table
+--- @return table
+function M.merge(t1, t2)
+  for k, v in pairs(t2) do
+    t1[k] = v
+  end
+  return t1
+end
+
 return M
