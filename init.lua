e3000bcb65bdeda238c90a54695a730e9abacf89
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-06-25 13:10:04 +0500

    (feat): Cut the line without yanking keymap

--- init.lua
+++ init.lua
@@ -39,6 +39,7 @@ require 'kickstart.mappings'
 
 vim.opt.hlsearch = true
 vim.keymap.set('n', 'x', '"_x', { desc = 'Delete character without yanking' })
+vim.keymap.set('n', '<M-x>', '"_dd', { desc = 'Cut the current line without yanking' })
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 vim.keymap.set(
