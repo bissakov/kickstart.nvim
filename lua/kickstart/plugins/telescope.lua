9fc65eb914f2f5e796af33764c07252ada5f6697
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-06-26 00:00:12 +0500

    (feat): Search hidden files with telescope

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -58,7 +58,12 @@ return {
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set(
+      'n',
+      '<leader>sf',
+      ':lua require"telescope.builtin".find_files({ hidden = true })<CR>',
+      { desc = '[S]earch [F]iles' }
+    )
     vim.keymap.set(
       'n',
       '<leader>ss',
