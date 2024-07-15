a07dab9c0a0b0ce027aa5eb21a6401fed1472305
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-07-16 00:57:02 +0500

    (feat): Add fixjson, cmake_format formatters and cmake_lint linter

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -4,6 +4,7 @@ return {
     local g = vim.g
     g.ale_ruby_rubocop_auto_correct_all = 1
     g.ale_linters = {
+      cmake = { 'cmake_lint' },
       c = { 'cpplint' },
       cpp = { 'cpplint' },
       lua = { 'selene' },
