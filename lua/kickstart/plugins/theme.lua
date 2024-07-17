3936f7dca25a85bc578fde957e1d6b52a701d2f5
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-07-17 23:11:28 +0500

    (feat): Install 'rebelot/kanagawa.nvim' color scheme and make it default

--- lua/kickstart/plugins/theme.lua
+++ lua/kickstart/plugins/theme.lua
@@ -1,10 +1,29 @@
 return {
-  'Mofiqul/vscode.nvim',
-  priority = 1000,
-  init = function()
-    require('vscode').setup()
+  {
+    'rebelot/kanagawa.nvim',
+    priority = 1000,
+    opts = {
+      dimInactive = true,
+      colors = {
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+      },
+    },
+    init = function()
+      require('kanagawa').setup()
 
-    vim.cmd.colorscheme 'vscode'
-    vim.cmd.hi 'Comment gui=none'
-  end,
+      vim.cmd.colorscheme 'kanagawa-dragon'
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  {
+    'Mofiqul/vscode.nvim',
+    priority = 1000,
+    init = function()
+      -- require('vscode').setup()
+
+      -- vim.cmd.colorscheme 'vscode'
+      -- vim.cmd.hi 'Comment gui=none'
+    end,
+  },
 }
