2cca49e6b9a5f950df22bd9e7b7f663e7fd559ff
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-07-30 23:44:25 +0500

    (feat): Add clang-tidy linter

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -6,9 +6,11 @@ return {
     g.ale_linters = {
       cmake = { 'cmake_lint' },
       c = { 'cpplint' },
-      cpp = { 'cpplint' },
+      cpp = { 'cpplint', 'clangtidy' },
       lua = { 'selene' },
       python = { 'ruff' },
     }
+
+    g.ale_cpp_clangtidy_extra_options = '-std=c++17 -W4 -Wunused-variable -I. -Isrc -Iinclude'
   end,
 }
