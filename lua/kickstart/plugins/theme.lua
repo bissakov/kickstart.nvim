50ac0cb9a0c23a3591a74145146ff021617d2dfe
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-09-07 20:09:17 +0500

    (feat): Remove 'Mofiqul/vscode.nvim'

--- lua/kickstart/plugins/theme.lua
+++ lua/kickstart/plugins/theme.lua
@@ -22,14 +22,4 @@ return {
       vim.cmd.hi 'Comment gui=none'
     end,
   },
-  {
-    'Mofiqul/vscode.nvim',
-    priority = 1000,
-    init = function()
-      -- require('vscode').setup()
-
-      -- vim.cmd.colorscheme 'vscode'
-      -- vim.cmd.hi 'Comment gui=none'
-    end,
-  },
 }
